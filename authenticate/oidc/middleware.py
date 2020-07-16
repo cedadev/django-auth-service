1718d74f31589f4ee0586953536d5eecd620cf3d
watucker <william.tucker@stfc.ac.uk>
2020-07-16 17:38:18 +0100

    Fixed deprecation error

--- authenticate/oidc/middleware.py
+++ authenticate/oidc/middleware.py
@@ -8,7 +8,7 @@ __license__ = "BSD - see LICENSE file in top-level package directory"
 
 import logging
 
-from authlib.client.errors import MismatchingStateError
+from authlib.integrations._client.errors import MismatchingStateError
 from authlib.common.errors import AuthlibBaseError
 from django.conf import settings
 
