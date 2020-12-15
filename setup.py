9a16c798ddd68596848aa2f02d05ece998f2bfd0
watucker <william.tucker@stfc.ac.uk>
2020-12-15 13:45:40 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open("README.md") as readme_file:
 
 setup(
     name="django-auth-service",
-    version="0.2.2",
+    version="0.2.3",
     description="Django application which authenticates and authorizes users.",
     author="William Tucker",
     author_email="william.tucker@stfc.ac.uk",
