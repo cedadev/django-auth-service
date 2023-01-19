c0befb6001c813050dbc8e3ca503cd1c7c22f7f0
watucker <william.tucker@stfc.ac.uk>
2023-01-19 21:26:14 +0000

    Added verbose logging when saving a username

--- authorize/views.py
+++ authorize/views.py
@@ -6,12 +6,16 @@ __copyright__ = "Copyright 2020 United Kingdom Research and Innovation"
 __license__ = "BSD - see LICENSE file in top-level directory"
 
 
+import logging
+
 from django.conf import settings
 from django.http import HttpResponse
 from django.views.generic import View
 
 from authenticate.utils import get_user
 
+
+LOG = logging.getLogger(__name__)
 
 DEFAULT_REMOTE_USER_RESPONSE_HEADER_KEY = "X-Remote-User"
 
@@ -35,6 +39,7 @@ class VerifyView(View):
 
             header_key = getattr(settings, "REMOTE_USER_RESPONSE_HEADER_KEY",
                 DEFAULT_REMOTE_USER_RESPONSE_HEADER_KEY)
+            LOG.debug(f"Saving user {user.username} to response: {header_key}")
             response[header_key] = user.username
 
         return response
