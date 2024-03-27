ff652b96d0b73639603f43b4770d0799ee886552
William Tucker <william.tucker@stfc.ac.uk>
2024-03-27 17:32:46 +0000

    Updated dependencies (now using a pyproject.toml)

--- setup.py
+++ setup.py
@@ -1,48 +1,6 @@
-""" A setuptools based setup module. """
+#!/usr/bin/env python
 
-__author__ = "William Tucker"
-__date__ = "2020-02-05"
-__copyright__ = "Copyright 2020 United Kingdom Research and Innovation"
-__license__ = "BSD - see LICENSE file in top-level directory"
+import setuptools
 
-
-from setuptools import setup, find_packages
-
-
-with open("README.md") as readme_file:
-    LONG_DESCRIPTION = readme_file.read()
-
-
-setup(
-    name="django-auth-service",
-    version="0.2.5",
-    description="Django application which authenticates and authorizes users.",
-    author="William Tucker",
-    author_email="william.tucker@stfc.ac.uk",
-    url="https://github.com/cedadev/django-auth-service",
-    long_description=LONG_DESCRIPTION,
-    long_description_content_type="text/markdown",
-    include_package_data=True,
-    packages=find_packages(),
-    install_requires=[
-        "Authlib",
-        "django",
-        "ndg-saml",
-        "ndg-httpsclient",
-        "pyopenssl",
-        "pyyaml",
-        "ua-parser",
-        "user-agents",
-        "requests",
-        "crypto-cookie",
-        "OPA-python-client",
-    ],
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "Intended Audience :: Developers",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-    ],
-    zip_safe=False,
-)
+if __name__ == "__main__":
+    setuptools.setup()
