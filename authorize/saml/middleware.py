f4bf5173f24f050f304c1bfb8e6760926a3fce45
William Tucker <william.tucker@stfc.ac.uk>
2025-02-08 18:34:44 +0000

    Update to id for authz

--- authorize/saml/middleware.py
+++ authorize/saml/middleware.py
@@ -36,7 +36,7 @@ class SAMLAuthorizationMiddleware(AuthorizationMiddleware):
 
         user = get_user(request)
         if user:
-            user_identifier = user.openid
+            user_identifier = user.username
             groups = user.groups
 
         LOG.debug(f"Querying authorization for resource: {resource}")
